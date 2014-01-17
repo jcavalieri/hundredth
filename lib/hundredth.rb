dd86fb0ac63874262c498c83db2a524def7eb7f1
John Cavalieri <john.cavalieri@gmail.com>
2014-01-16 20:05:13 -0600

    stubbed out gem files

---
+++ lib/hundredth.rb
@@ -0,0 +1,12 @@
+.name        = 'hundredth'
+  s.version     = '0.0.1'
+  s.date        = '2014-01-16'
+  s.summary     = "writes out ordinal numbers"
+  s.description = "Writes out 1 to 100 full words. 101 and greater are are written out using the number followed by the ordinal indicator, e.g. 'st', 'nd', 'rd'."
+  s.authors     = ["John Cavalieri", "Rosie Hoyem"]
+  s.email       = 'jcavalieri@fastpencil.com'
+  s.files       = ["lib/hundredth.rb"]
+  s.homepage    = 
+    'http://github.com/jcavalieri/hundredth'
+  s.license       = 'MIT'
+end
