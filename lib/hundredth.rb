f0092c91bf2e2cd77b9f143a4bfd0c5aba3405ea
John Cavalieri <john.cavalieri@gmail.com>
2014-03-06 23:11:21 -0600

    Added better localization support Added french ordinals

--- lib/hundredth.rb
+++ lib/hundredth.rb
@@ -1,15 +1,23 @@
 class Hundredth
   require 'i18n'
 
-  I18n.config.enforce_available_locales = false
   @@gem_root = Gem.loaded_specs['hundredth'].full_gem_path
-
   I18n.load_path += Dir.glob(File.join(@@gem_root, 'lib/locales/*.yml'))
   I18n.backend.load_translations
+  I18n.config.enforce_available_locales = true
+  I18n.config.default_locale = :en
 
-  def self.ordinal(number)
-    I18n.t 'hundredth.num_' + number.to_s
+  class << self
+    def ordinal(number, locale = Thread.current[:locale] || I18n.config.default_locale)
+      I18n.t 'hundredth.num_' + number.to_s, locale: locale
+    end
+
+    def default_locale
+      I18n.config.default_locale
+    end
+
+    def default_locale=(value)
+      I18n.config.default_locale = value
+    end
   end
-
-
 end
